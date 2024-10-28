a655cb20cc27e62de8c4045a58ad157101d4bcf8
cexar123 <wilianverdeloyola@gmail.com>
2024-10-28 02:01:22 -0500

    MOV123

--- MADERERA/VentaBuscarCliente.Designer.cs
+++ MADERERA/VentaBuscarCliente.Designer.cs
@@ -38,9 +38,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 77);
+            this.label1.Location = new System.Drawing.Point(30, 62);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(155, 20);
+            this.label1.Size = new System.Drawing.Size(127, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "BUSCAR POR DNI :";
             // 
@@ -48,39 +48,42 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 22);
+            this.label2.Location = new System.Drawing.Point(11, 18);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(334, 41);
+            this.label2.Size = new System.Drawing.Size(287, 34);
             this.label2.TabIndex = 1;
             this.label2.Text = "Seleccione al cliente :";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(38, 120);
+            this.dataGridView1.Location = new System.Drawing.Point(34, 96);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 62;
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(762, 320);
+            this.dataGridView1.Size = new System.Drawing.Size(677, 256);
             this.dataGridView1.TabIndex = 2;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(195, 71);
+            this.textBox1.Location = new System.Drawing.Point(180, 56);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(151, 26);
+            this.textBox1.Size = new System.Drawing.Size(135, 22);
             this.textBox1.TabIndex = 3;
             // 
             // VentaBuscarCliente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PaleTurquoise;
-            this.ClientSize = new System.Drawing.Size(854, 478);
+            this.ClientSize = new System.Drawing.Size(759, 382);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MinimizeBox = false;
             this.Name = "VentaBuscarCliente";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
