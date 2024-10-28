fa8df2303bda192f63d6647d0984262e1679ddb1
Bruno Aaron Julca Alayo <N00363100@upn.pe>
2024-10-28 16:16:15 -0500

    acctualizacion

--- MADERERA/CoreVenta.cs
+++ MADERERA/CoreVenta.cs
@@ -16,5 +16,23 @@ namespace CAPAPRESENTACION
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VentaBuscarCliente buscarCliente = new VentaBuscarCliente();
+            buscarCliente.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            VentaBuscarVendedor buscarvend = new VentaBuscarVendedor();
+            buscarvend.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            VentaBuscarProducto buscarpro = new VentaBuscarProducto();
+            buscarpro.ShowDialog();
+        }
     }
 }
