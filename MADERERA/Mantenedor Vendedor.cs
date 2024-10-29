ad5349085efdb5d0b0f54f848ac6887baad1c42d
JUAN <espinozaquispejuancarlos.kepler@gmail.com>
2024-10-28 23:35:22 -0500

    modifique el mantenedor vendedor

--- MADERERA/Mantenedor Vendedor.cs
+++ MADERERA/Mantenedor Vendedor.cs
@@ -16,5 +16,10 @@ namespace CAPAPRESENTACION
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
