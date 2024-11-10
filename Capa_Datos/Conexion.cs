9683444c0f7bd7006cd1584b5cca7d82c163b084
Bruno Aaron Julca Alayo <N00363100@upn.pe>
2024-11-10 13:26:04 -0500

    cambios

--- Capa_Datos/Conexion.cs
+++ Capa_Datos/Conexion.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Capa_Datos
 {
-    internal class Conexion
+    public class Conexion
     {
+        
     }
 }
---
+++ Capa_Entidad/E_Permisos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Entidad
+{
+    public class E_Permisos
+    {
+        public int IdPermisos { get; set; }
+        public E_Rol Rid { get; set; }
+        public string NombreMenu { get; set; }
+        public string FechaRegistro     { get; set; }
+    }
+}
